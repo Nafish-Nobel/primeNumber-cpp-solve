d0356754509f2e83b0af8333eaf6ca27e4a74b4c
Nur Nafish Ahmed Nobel <86622356+Nafish-Nobel@users.noreply.github.com>
2023-04-18 18:11:29 +0600

    input 10_Not Prime

--- new-tests/primeNumber.cpp
+++ new-tests/primeNumber.cpp
@@ -6,4 +6,13 @@ int main()
     cout<< "input a number: " <<endl;
     cin>> n;
 
+    int i =2;
+    while(i<n){
+        if (n%i==0)
+        {
+            cout<< "Not prime"<<endl; 
+        }
+        
+        i= i+1;
+    }
 }

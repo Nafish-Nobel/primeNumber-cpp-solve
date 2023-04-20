49056e7a3583ea7f2fa8448ec1417c782de0cc1f
Nur Nafish Ahmed Nobel <86622356+Nafish-Nobel@users.noreply.github.com>
2023-04-20 18:15:14 +0600

    b

--- new-tests/patterns-prac.cpp
+++ new-tests/patterns-prac.cpp
@@ -7,18 +7,43 @@ int main(){
     cin>> n;
 
     int i = 1;
-
-    while (i<=n)
-    {
-        int j =1;
+    int count = 1;
+    while(i<=n){
+        int j = 1;
         while (j<=n)
         {
-            cout<<j; //for reverse//n-j+1
-            j = j +1;
+            
+        cout<<count<< " ";
+        count = count + 1;
+        j = j + 1;
         }
         cout<<endl;
         i = i + 1;
         
     }
+
+
+//********************************************//
+
+    // int n;
+    // cout<< "enter your number: " <<endl;
+    // cin>> n;
+
+    // int i = 1;
+
+    // while (i<=n)
+    // {
+    //     int j =1;
+    //     while (j<=n)
+    //     {
+    //         cout<<j; //for reverse//n-j+1
+    //         j = j +1;
+    //     }
+    //     cout<<endl;
+    //     i = i + 1;
+        
+    // }
+
+    //********************************************//
     
 }

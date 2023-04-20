231eae0cebec2e4496f7a8dff29604563e26292e
Nur Nafish Ahmed Nobel <86622356+Nafish-Nobel@users.noreply.github.com>
2023-04-20 23:23:34 +0600

    tringle

--- new-tests/patterns-prac.cpp
+++ new-tests/patterns-prac.cpp
@@ -2,25 +2,61 @@
 using namespace std;
 
 int main(){
-    int n;
-    cout<< "enter your number: " <<endl;
-    cin>> n;
 
-    int i = 1;
-    int count = 1;
-    while(i<=n){
-        int j = 1;
-        while (j<=n)
+    int n;
+    cout<< "enter a number: ";
+    cin>>n;
+
+    int row = 1;
+
+    while (row<=n)
+    {
+        int col =1;
+        while (col<=row)
         {
-            
-        cout<<count<< " ";
-        count = count + 1;
-        j = j + 1;
+            cout<< "*";
+            col = col + 1;
         }
-        cout<<endl;
-        i = i + 1;
         
+        cout<<endl;
+        row = row +1;
     }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//********************************************//
+
+    // int n;
+    // cout<< "enter your number: " <<endl;
+    // cin>> n;
+
+    // int i = 1;
+    // int count = 1;
+    // while(i<=n){
+    //     int j = 1;
+    //     while (j<=n)
+    //     {
+            
+    //     cout<<count<< " ";
+    //     count = count + 1;
+    //     j = j + 1;
+    //     }
+    //     cout<<endl;
+    //     i = i + 1;
+        
+    // }
 
 
 //********************************************//

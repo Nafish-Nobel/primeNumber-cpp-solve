be4712f992fdc69e7a645630bad5190c7233e487
Nur Nafish Ahmed Nobel <86622356+Nafish-Nobel@users.noreply.github.com>
2023-05-31 20:22:55 +0600

    l9

---
+++ L9/ArryIntro.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+//arry function
+void printArry(int arr[], int size){
+    //printing the arry
+    cout << "printing the arry" <<endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout<< arr[i] << " ";
+    }
+    cout<< "printing Done" <<endl;
+    
+}
+
+int main(){
+    int number[15];
+    cout<< "value at 14 index: " << number[14] <<endl;
+
+    int second[3]={5,7,11};
+    cout<< "value at 2 index: " <<second[2] <<endl;
+
+
+
+}
---
+++ L9/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+void printArry(int arr[], int size){
+//     //printing the arry
+//     cout << "printing the arry" <<endl;
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout<< arr[i] << " ";
+//     }
+//     cout<< "printing Done" <<endl;
+    
+// }

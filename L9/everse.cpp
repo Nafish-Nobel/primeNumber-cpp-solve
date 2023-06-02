dece46326be7536ec48e36546314ede905fec093
Nur Nafish Ahmed Nobel <86622356+Nafish-Nobel@users.noreply.github.com>
2023-06-02 19:28:38 +0600

    updat

--- L9/everse.cpp
+++ L9/everse.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 
+void reverse(int arr[], int n){
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end){
+        swap(arr[start], arr[end]);
+
+        start ++ ;
+        end --;
+    }
+}
+
+
 void printArry(int arr[], int n){
     for (int i = 0; i < n; i++)
     {

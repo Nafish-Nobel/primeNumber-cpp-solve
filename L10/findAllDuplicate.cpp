745fc14d0e6a6d26fe69669dcae2f40475dc49f7
Nur Nafish Ahmed Nobel <86622356+Nafish-Nobel@users.noreply.github.com>
2023-06-05 23:31:51 +0600

    done

---
+++ L10/findAllDuplicate.cpp
@@ -0,0 +1,19 @@
+//442. Find All Duplicates in an Array-- leetcode
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        if(nums.empty())return {};
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]!=nums[j])continue;
+                else{
+                    ans.push_back(nums[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
